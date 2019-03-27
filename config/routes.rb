99f353a9783b0993bdcd5b8548d997ea9beb1c39
sbstn.jmnz <sbstn.jmnz@yahoo.com>
2019-03-27 12:32:25 -0300

    ruta root a gas_starions#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :gas_stations, except: :destroy
+  resources :gas_stations
+  root to:'gas_stations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
